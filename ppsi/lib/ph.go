4bf2fec0b686760f82a4735af4f209526b95246e
lihiid <lihi.idan@yale.edu>
2017-01-22 11:58:25 -0800

    Create ph.go

---
+++ ppsi/lib/ph.go
@@ -0,0 +1,75 @@
+package lib
+
+
+import (
+	"github.com/dedis/crypto/abstract"
+	"github.com/dedis/crypto/random"
+)
+
+type PH struct {
+	encKey abstract.Scalar
+	decKey abstract.Scalar
+	suite  abstract.Suite
+}
+
+func NewPH(suite abstract.Suite) *PH {
+	ph := &PH{
+		suite: suite,
+	}
+	ph.createKeys()
+	return ph
+}
+
+//Create encryption and decryption keys
+func (c *PH) createKeys() {
+
+	enckey := c.suite.Scalar().Pick(random.Stream) // ephemeral private key
+
+	for !c.suite.Scalar().Gcd(enckey).Equal(c.suite.Scalar().One()) {
+		enckey = c.suite.Scalar().Pick(random.Stream)
+	}
+
+	c.encKey = enckey
+	c.decKey = c.suite.Scalar().Inv(enckey)
+
+}
+
+//Decrypt with Pohlig Hellman, output is a string
+func (c *PH) PHDecrypt(cipher abstract.Point) (
+	message string) {
+
+	var bytemessage []byte
+
+	S := c.suite.Point().Mul(cipher, c.decKey)
+	bytemessage, _ = S.Data()
+	message = string(bytemessage)
+
+	return
+
+}
+
+//Encrypt with Pohlig Hellman, input is []byte
+func (c *PH) PHEncrypt(message []byte) (
+	S abstract.Point) {
+
+	M, _ := c.suite.Point().Pick(message, random.Stream)
+	S = c.suite.Point().Mul(M, c.encKey)
+	return
+}
+
+//Decrypt with Pohlig Hellman, output is a point
+func (c *PH) PartialPHDecrypt(cipher abstract.Point) (
+	S abstract.Point) {
+
+	S = c.suite.Point().Mul(cipher, c.decKey)
+	return
+
+}
+
+//Encrypt with Pohlig Hellman, input is a point
+func (c *PH) PartialPHEncrypt(M abstract.Point) (
+	S abstract.Point) {
+
+	S = c.suite.Point().Mul(M, c.encKey)
+	return
+}
